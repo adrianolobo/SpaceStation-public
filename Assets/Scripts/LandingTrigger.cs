ce4e86484706a415af827f2acf039f1fdaf21dbb
Adriano Lobo <adrianolobo@gmail.com>
2020-04-13 23:42:09 -0300

    creagint LandingTrigger and StationLanding

---
+++ Assets/Scripts/LandingTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LandingTrigger : MonoBehaviour
+{
+    private StationLanding stationLanding;
+    void Start()
+    {
+        stationLanding = GetComponentInParent<StationLanding>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("YAYYYYY");
+        Debug.Log(collision.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/StationLanding.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StationLanding : MonoBehaviour
+{
+    public void LandCarrier(SpaceCarrier carrier)
+    {
+        
+    }
+}
