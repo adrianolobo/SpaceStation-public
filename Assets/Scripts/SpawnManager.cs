1c0824a3e85cd1690f96603eb276436e5e7f1a84
Adriano Lobo <adrianolobo@gmail.com>
2020-04-20 23:58:59 -0300

    creating Spawn Manager

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    private List<string> spawnOrder;
+    // Start is called before the first frame update
+    void Start()
+    {
+        resetSpawnOrder();
+        Debug.Log(spawnOrder);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void resetSpawnOrder()
+    {
+        spawnOrder = new List<string> { "top", "right", "bottom", "left" };
+        for (int i = 0; i < spawnOrder.Count; i++)
+        {
+            string temp = spawnOrder[i];
+            int randomIndex = Random.Range(i, spawnOrder.Count);
+            spawnOrder[i] = spawnOrder[randomIndex];
+            spawnOrder[randomIndex] = temp;
+        }
+    }
+}
