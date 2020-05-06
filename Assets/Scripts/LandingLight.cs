b909e091e0decd7ccc71a3ef2f3f0301e770857c
Adriano Lobo <adrianolobo@gmail.com>
2020-05-05 23:52:35 -0300

    creating landing-light

---
+++ Assets/Scripts/LandingLight.cs
@@ -0,0 +1,11 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class LandingLight : MonoBehaviour
+{
+    private void Start()
+    {
+        transform.localScale = new Vector3(0.2f, 0.2f, 1);
+        transform.DOScale(new Vector3(1, 1, 0), 0.6f).SetEase(Ease.InOutCubic).SetLoops(-1, LoopType.Restart);
+    }
+}
