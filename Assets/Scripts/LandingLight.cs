29f3383314160dad4902d9bdc879ca3dc1136ae5
Adriano Lobo <adrianolobo@gmail.com>
2020-05-06 23:30:50 -0300

    creating landing lights

--- Assets/Scripts/LandingLight.cs
+++ Assets/Scripts/LandingLight.cs
@@ -3,9 +3,13 @@ using UnityEngine;
 
 public class LandingLight : MonoBehaviour
 {
+    public float startDelay = 0;
     private void Start()
     {
-        transform.localScale = new Vector3(0.2f, 0.2f, 1);
-        transform.DOScale(new Vector3(1, 1, 0), 0.6f).SetEase(Ease.InOutCubic).SetLoops(-1, LoopType.Restart);
+        transform.localScale = new Vector3(0.1f, 0.1f, 1);
+        transform.DOScale(new Vector3(0.6f, 0.6f, 0), 0.65f)
+            .SetDelay(startDelay)
+            .SetEase(Ease.InOutCubic)
+            .SetLoops(-1, LoopType.Restart);
     }
 }
