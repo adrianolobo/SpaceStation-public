ecca0f58f5fc46789784c3f155b1c25786c7259f
Adriano Lobo <adrianolobo@gmail.com>
2020-06-01 23:39:35 -0300

    creating the menu item

---
+++ Assets/Scripts/Menu/MenuStationLevel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MenuStationLevel : MonoBehaviour
+{
+    public SpaceStation spaceStation;
+
+    private void Start()
+    {
+        TextMeshProUGUI title = GetComponentInChildren<TextMeshProUGUI>();
+        title.SetText(spaceStation.stationName);
+
+    }
+}
--- Assets/Scripts/SpaceStation/SpaceStation.cs
+++ Assets/Scripts/SpaceStation/SpaceStation.cs
@@ -7,4 +7,6 @@ public class SpaceStation : Singleton<SpaceStation>
     public float[] spawnChances;
     public float cargoPercentage1;
     public float cargoPercentage2;
+    public string stationName;
+    public Sprite stationImage;
 }
